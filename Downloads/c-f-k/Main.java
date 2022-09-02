64829a984fa2d086885613cf657a29091b37c9be
Kader Eren <kader.eren42@gmail.com>
2022-09-02 17:54:16 +0300

    second commit

--- Downloads/c-f-k/Main.java
+++ Downloads/c-f-k/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Celcius cel = new Celcius(30);
         System.out.println(cel.getDegree() + " celcius = " + cel.convert(DegreeType.Kelvin) + " Kelvin");
+        System.out.println(cel.getDegree() + " celcius = " + cel.convert(DegreeType.Celcius) + " Celcius");
+        System.out.println(cel.getDegree() + " celcius = " + cel.convert(DegreeType.Fahrenheit) + " Fahrenheit");
+
     }
 }
